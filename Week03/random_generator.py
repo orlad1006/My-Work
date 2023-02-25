f8453e2850da9933b92604345b95d4b702fa925e
Wayne O'Brien <orlad1006@gmail.com>
2023-02-25 20:25:32 +0000

    labs 3.1 random generator

---
+++ Week03/random_generator.py
@@ -0,0 +1,7 @@
+# random_generator.py
+# Author: Orla Dowling
+# program that prints out a random number between 1 and 10
+
+import random
+number = random.randint(1,10)
+print ("here is a random number {}".format(number))
