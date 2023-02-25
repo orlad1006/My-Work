e8c7f4d3f13bc4ecf8c31adc56eab041481b5bb6
Wayne O'Brien <orlad1006@gmail.com>
2023-02-25 19:36:49 +0000

    test of types

---
+++ Week03/lab3.1test_types.py
@@ -0,0 +1,27 @@
+#3.1 test of types
+# create different variable types as below
+# integer
+#float
+#boolean
+#string
+#list
+#use type function to check the type class of variables
+#Author: Orla Dowling
+
+
+
+i = 3
+fl = 3.5
+isa = True
+memo = 'how now brown cow'
+lots = []
+
+print ('Variable {} is of type:{} and value:{}'.format('i', type(i), i))
+print ('Variable {} is of type:{} and value:{}'.format('fl', type(fl), fl))
+print ('Variable {} is of type:{} and value:{}'.format('is', type(isa), isa))
+print ('Variable {} is of type:{} and value:{}'.format('memo', type(memo), memo))
+print ('Variable {} is of type:{} and value:{}'.format('lots', type(lots),lots))
+
+
+
+
---
+++ Week03/messingwithtypes.py
@@ -0,0 +1,11 @@
+#week03variables
+#messingwith types
+#Author:Orla Dowling
+
+
+answer_of_question = 12
+print(f"answer is {answer_of_question}")
+
+
+
+
