75be2a43ef41d88cd9ea0381ff4f10c12d581bd4
Wayne O'Brien <orlad1006@gmail.com>
2023-03-02 12:48:56 +0000

    lab3.2.length.py

---
+++ Week03/labs3.3.length.py
@@ -0,0 +1,10 @@
+# labs.3.2.length.py
+#Author: Orla Dowling
+#This program reads in a String and outputs its length
+
+
+input_string = input('Please enter a string:')
+lenght_of_string = len(input_string)
+
+print("The length of {} is {}".format(input_string,lenght_of_string))
+
