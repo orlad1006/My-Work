bb593ba7d2ea2cf9758db7d3379ce3ac201cd77d
Wayne O'Brien <orlad1006@gmail.com>
2023-03-08 12:51:51 +0000

    Forloops

---
+++ Week04_flow/forloops.py
@@ -0,0 +1,25 @@
+#for loops
+#Author: Orla Dowling
+
+#write a program taht keeps reading the number until the user enters 0
+#the program should append each of them to a list
+#Print out each of the numbers entered and the average of them (use a list)
+
+numbers = []
+# first number then we check if it is 0 in the while loop
+
+number = int(input("enter number (0 to quit) "))
+
+while number != 0:
+    numbers.append(number)
+
+    # read the next number and check if 0
+    number = int(input(" enter another (0 to quit)"))
+
+for value in numbers:
+    print (value)
+
+# i want average to be a float
+
+average = float(sum(numbers))/len(numbers)
+print(f"the average is {average}")
