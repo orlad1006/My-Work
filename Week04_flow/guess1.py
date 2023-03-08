f8e2dd0f75c97d748ade1607b9e27dd692fb5f6a
Wayne O'Brien <orlad1006@gmail.com>
2023-03-08 12:32:26 +0000

    guess1

---
+++ Week04_flow/guess1.py
@@ -0,0 +1,17 @@
+# guess.py
+# Author: Orla Dowling
+
+#write a program that prompts the user to guess a number, the program should keep prompting the user to 
+#guess the number until  the user get the right one
+
+
+number_to_guess = 30 
+
+guess = int(input("Please guess the number:"))
+while guess != number_to_guess:
+    print ("Wrong")
+    guess = int(input("Please guess again: "))
+
+    print ("Well done! Yes the numbwe was", number_to_guess)
+
+    
