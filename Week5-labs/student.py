5ba6746fff6c9c8daf3c30de203a9f813bf1fbd1
Wayne O'Brien <orlad1006@gmail.com>
2023-04-06 08:55:26 +0100

    week 5 student

---
+++ Week5-labs/student.py
@@ -0,0 +1,24 @@
+#Title: student.py
+#Author: Orla Dowling
+#Task: Write a program that stores a student name and a list of her courses and
+#grades in a dict, the program should then print out her data.
+#The number of course she has could change.
+
+
+student = {
+"name":"Mary",
+"modules": [
+{
+"courseName":"Programming",
+"grade": 45
+},
+{
+"courseName":"History",
+"grade":99
+}
+]
+}
+print ("Student: {}".format(student["name"]))
+for module in student["modules"]:
+    print ("\t {} \t: {}".format(module["courseName"], module["grade"]))
+
